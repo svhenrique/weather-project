635da9f12aa5d048ca18fd1937d2ea48fe08994d
Silvio <silviohenrique1234@hotmail.com>
2020-10-26 17:37:55 -0300

    fixed secret_key

--- weather/settings.py
+++ weather/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-&bdptfyj=@2kksp#60-s&&u$vk&k^!so@3oizqj@^v_em==3'  # put a secret key
+SECRET_KEY = '+-&bdptfyj=@2kksp#60-s&&u$vk&k^!so@3oizqj@^v_em==3'  # put a secret key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
