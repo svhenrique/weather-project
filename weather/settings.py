5816f37e8a7a8aad075b3683ea3d6458434cae06
Silvio <silviohenrique1234@hotmail.com>
2020-10-26 17:43:33 -0300

    fixed root variables

--- weather/settings.py
+++ weather/settings.py
@@ -122,5 +122,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
