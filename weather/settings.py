39b4ac3c4bd5950757c173f34eaf959afc6e5b81
Silvio <silviohenrique1234@hotmail.com>
2020-10-30 14:58:31 -0300

    finished the settings config for deploy

--- weather/settings.py
+++ weather/settings.py
@@ -97,7 +97,6 @@ DATABASES = {
 DATABASES = {}
 DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
-
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
